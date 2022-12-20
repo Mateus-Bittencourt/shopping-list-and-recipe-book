802b9e1475f06d99c79308a33a59f8a533d34a15
Mateus Bittencourt <mateusmbittencourt@gmail.com>
2022-12-20 13:54:47 -0300

    add stuf

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,14 +1,39 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { AuthService } from './auth.service';
 
 @Component({
-  selector: "app-auth",
-  templateUrl: "./auth.component.html",
-  styleUrls: ["./auth.component.scss"],
+  selector: 'app-auth',
+  templateUrl: './auth.component.html',
+  styleUrls: ['./auth.component.scss'],
 })
 export class AuthComponent {
   isLoginMode = true;
 
+  constructor(private authService: AuthService) {}
+
   onSwitchMode() {
     this.isLoginMode = !this.isLoginMode;
+  }
+  onSubmit(form: NgForm) {
+    if (!form.valid) {
+      return;
+    }
+    const email = form.value.email;
+    const password = form.value.password;
+
+    if (this.isLoginMode) {
+      // ...
+    } else {
+      this.authService.signup(email, password).subscribe(
+        (responseData) => {
+          console.log(responseData);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+    }
+    form.reset();
   }
 }
